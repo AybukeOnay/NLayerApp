625ae90cd062704e018a3b8eacb779bda0addbbc
Aybuke Onay <aybuke.onay7@gmail.com>
2022-11-27 17:01:38 +0300

    Core layer added

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+
+        //productRepository.GetAll(x=>x.id>5).ToList();
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+
+        //productRepository.where(x=>x.id>5).OrderBy.ToListAsync();
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+
+        Task AddAsync(T entity);
+
+        Task AddRangeAsync(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entities);
+       
+    }
+}
