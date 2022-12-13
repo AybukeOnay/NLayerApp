d5ab2af0c94e05644f15a6d48c36d57e38b0a859
Aybuke Onay <aybuke.onay7@gmail.com>
2022-12-13 22:41:56 +0300

    appDbContext and EntityConfiguration added

---
+++ NLayer.Repository/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Configurations
+{
+    internal class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x=> x.Id).UseIdentityColumn();
+            builder.Property(x=> x.Name).IsRequired().HasMaxLength(50);
+
+            //Tablonun adını yazdırmayı sağlar
+            builder.ToTable("Categories");
+        }
+    }
+}
